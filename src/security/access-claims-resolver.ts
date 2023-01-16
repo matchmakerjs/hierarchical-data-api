fe269f0e26374afacf590ce4aeb161d33b75b85a
Olaleye Afolabi <olaleyeone@gmail.com>
2023-01-16 09:10:41 +0100

    dependency update

--- src/security/access-claims-resolver.ts
+++ src/security/access-claims-resolver.ts
@@ -1,39 +1,32 @@
-import {
-  AccessClaimsResolver,
-  BearerTokenClaimsResolver,
-  JwtClaims,
-  JwtValidator,
-} from "@matchmakerjs/jwt-validator";
+import { AccessClaimsResolver, BearerTokenClaimsResolver, ErrorSender, JwtClaims, JwtValidator } from "@matchmakerjs/jwt-validator";
 import { IncomingMessage } from "http";
 
 export const getCookieValue = (req: IncomingMessage, cookieName: string) => {
-  const entries: string[] = req.headers.cookie?.split(/ *; */);
-  for (const entry of entries) {
-    const [name, value] = entry.split(/ *= */);
-    if (name === cookieName) {
-      return value;
+    const entries: string[] = req.headers.cookie?.split(/ *; */) || [];
+    for (const entry of entries) {
+        const [name, value] = entry.split(/ *= */);
+        if (name === cookieName) {
+            return value;
+        }
     }
-  }
-};
+}
 
-export const getAccessTokenFromCookie = (req: IncomingMessage) =>
-  getCookieValue(req, process.env.ACCESS_TOKEN_COOKIE_NAME || "access_token");
+export const getAccessTokenFromCookie = (req: IncomingMessage) => getCookieValue(req, process.env.ACCESS_TOKEN_COOKIE_NAME || 'access_token');
 
-export class BearerTokenAccessClaimsResolverWithCookieSupport
-  implements AccessClaimsResolver<IncomingMessage>
-{
-  bearerClaimsResolver: AccessClaimsResolver<string>;
+export class BearerTokenAccessClaimsResolverWithCookieSupport implements AccessClaimsResolver<IncomingMessage>{
 
-  constructor(private jwtValidator: JwtValidator) {
-    this.bearerClaimsResolver = new BearerTokenClaimsResolver(jwtValidator);
-  }
+    bearerClaimsResolver: AccessClaimsResolver<string>;
 
-  async getClaims(req: IncomingMessage): Promise<JwtClaims> {
-    const authHeader = req.headers["authorization"];
-    if (authHeader) {
-      return this.bearerClaimsResolver.getClaims(authHeader);
+    constructor(private jwtValidator: JwtValidator) {
+        this.bearerClaimsResolver = new BearerTokenClaimsResolver(jwtValidator);
     }
-    const accessToken = getAccessTokenFromCookie(req);
-    if (accessToken) return this.jwtValidator.validateBearerToken(accessToken);
-  }
+
+    async getClaims(req: IncomingMessage, errorSender: ErrorSender): Promise<JwtClaims> {
+        const authHeader = req.headers['authorization'];
+        if (authHeader) {
+            return this.bearerClaimsResolver.getClaims(authHeader, errorSender);
+        }
+        let accessToken = getAccessTokenFromCookie(req);
+        if (accessToken) return this.jwtValidator.validateBearerToken(accessToken, errorSender);
+    }
 }
